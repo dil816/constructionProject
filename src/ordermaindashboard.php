99f590e489ad55fe5844ae4ef10ae38c2e63bffc
Deshan Kodithuwakku <62970260+Deshankodithuwakku@users.noreply.github.com>
2024-05-05 17:26:54 +0530

    done

--- src/ordermaindashboard.php
+++ src/ordermaindashboard.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Order Dashboard</title>
   <link rel="stylesheet" href="./style/ordermaindashboard.css">
 
   <link href="https://cdn.jsdelivr.net/npm/daisyui@4.10.5/dist/full.min.css" rel="stylesheet" type="text/css" />
