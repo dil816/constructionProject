1a60f4c901993ee28e7946967bd490c57dcda3cf
Deshan Kodithuwakku <62970260+Deshankodithuwakku@users.noreply.github.com>
2024-05-05 16:53:22 +0530

    all done

--- src/login.php
+++ src/login.php
@@ -48,7 +48,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 <head>
 
   <meta charset="UTF-8">
-  <title>Sign Up</title>
+  <title>Login</title>
   <link rel="stylesheet" href="./style/login.css" />
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
